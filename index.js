5c48a98637f2268452b52c86935b7329e0ba2250
sachinbedke <126045456+sachinbedke@users.noreply.github.com>
2024-02-09 19:05:43 +0530

    ui added

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ app.use(express.json())
 app.use(cors())
 
 app.use(express.static(path.join(__dirname, "dist")))
+
+app.use("/api/admin", require("./router/todoRoute"))
+
 app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
-app.use("/api/admin", require("./router/todoRoute"))
 mongoose.connection.once("open", () => {
     console.log("Mongoose Connected");
     app.listen(5000, console.log("server runinngs"))
